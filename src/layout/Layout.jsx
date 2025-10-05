83b4cb200f1d565d73f58c41cf80ecaf23e0d4ea
jobit91 <157335465+jobit91@users.noreply.github.com>
2025-10-05 14:24:47 +0200

    update layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -7,15 +7,14 @@ export default function Layout() {
   const bannerGames = useLoaderData(); 
   return (
     <div className="style-layout-system">
-      //particles
+      {/* PARTICLES BACKGROUND */}
       <ParticlesBackground />
-      
+      {/* NAVBAR - BANNER - TITOLO */}
       <Header bannerGames={bannerGames} /> 
-           
       <div className="main-container">
         <Outlet />
       </div>
-
+      {/* FOOTER */}
       <Footer />
     </div>
   );
