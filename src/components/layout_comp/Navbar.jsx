6cbea80db8d32be153adde22aa30cabd5c300b5e
jobit91 <157335465+jobit91@users.noreply.github.com>
2025-10-05 16:22:12 +0200

    nav fix

--- src/components/layout_comp/Navbar.jsx
+++ src/components/layout_comp/Navbar.jsx
@@ -74,7 +74,9 @@ export default function Navbar() {
                 <div className="container-fluid">
                     
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+    <i className="fas fa-bars"></i>
+</button>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav mx-auto">
