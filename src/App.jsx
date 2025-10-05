2952393baac31336e43deda69c905159c64b7be6
jobit91 <157335465+jobit91@users.noreply.github.com>
2025-10-05 13:36:27 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import '@fontsource/orbitron/900.css'; 
-import '@fontsource/orbitron/700.css';
-import '@fontsource/rajdhani/400.css'; 
-import '@fontsource/rajdhani/500.css'; 
-import '@fontsource/rajdhani/600.css'; 
-import '@fontsource/rajdhani/700.css'; 
-
 import { useState, useEffect } from 'react'
 import './App.css'
-import TextSpawner from './components/TextSpawner'
 import { Routing } from './routes/Routing'
 import SessionProvider from './context/SessionProvider'
 
